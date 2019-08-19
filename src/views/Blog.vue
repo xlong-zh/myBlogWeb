c6cb11a607c8010946752804f357d84960365923
xlong-zh <48941696+xlong-zh@users.noreply.github.com>
2019-08-19 23:40:38 +0800

    Update Blog.vue

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="blog py-50 bg-white">
     <div v-for="(item,i) in model" :key="i">
-      <Art :essay="item">555</Art>
-      <Art :essay="item">666</Art>
+      <Art :essay="item">qweqwzxa</Art>
       <div class="border-bottom" style="width:500px;margin:0 auto;"></div>
     </div>
   </div>
